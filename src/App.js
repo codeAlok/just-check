b819055eb1f84a8f9e1ec6a2daa5c75a51bbe952
alok <alok60541@gmail.com>
2022-12-26 22:22:38 +0530

    bootstrap pagination done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import ReactPaginate from 'react-paginate';
 
 function App() {
+  
+  const handlePageClick = (data) => {
+    console.log(data.selected);
+  }
+
   return (
-    <div className="App">
-      
+    <div>
+
+      {/* all classnames used from bootstrap pagination */}
+      <ReactPaginate
+       previousLabel= {'previous'}
+       nextLabel= {'next'}
+       breakLabel= {'...'}
+       pageCount= {25}
+       marginPagesDisplayed= {2}
+       pageRangeDisplayed= {3}
+       onPageChange= {handlePageClick}
+       containerClassName= {'pagination justify-content-center'}
+       pageClassName= {'page-item'}
+       pageLinkClassName= {'page-link'}
+       previousClassName= {'page-item'}
+       previousLinkClassName= {'page-link'}
+       nextClassName= {'page-item'}
+       nextLinkClassName= {'page-link'} 
+       breakClassName= {'page-item'}
+       breakLinkClassName= {'page-link'}
+       activeClassName= {'active'}
+      />
+
     </div>
   );
 }
